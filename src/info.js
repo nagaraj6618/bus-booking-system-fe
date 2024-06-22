c574a4f3792ac5476a4f6bfddb8fdcecce728306
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-22 08:38:13 +0530

    Fix : changed logo and name

--- src/info.js
+++ src/info.js
@@ -1,4 +1,6 @@
 
 
-export const BE_URL = "http://localhost:8000/api/v1";
+// export const BE_URL = "http://localhost:8000/api/v1";
+
+export const BE_URL = "https://bus-booking-system-eight.vercel.app/api/v1"
  
